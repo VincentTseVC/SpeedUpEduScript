0edd6aaf0f27e381aa690a8badacf540a52d53dd
VincentTse <from_vincent@hotmail.com>
2022-10-25 15:46:07 +0800

    changed cleaner

---
+++ game_functions.py
@@ -0,0 +1,27 @@
+
+def func1(a:int,  b:  str)-> str:
+    """aaa
+
+    >>>func1(1,'a')
+    """
+    a=a+b or  2
+    c = a   -b %1
+    d= 1*2
+    word ="s"
+    return a
+
+def func2():
+    a = a+1
+    b *=3
+    return 1 ==2
+def func3():
+    return 1>=   2  and   3>5
+
+# test
+def func4():
+    pass
+# test2
+# test3
+#
+def func5():
+    pass
