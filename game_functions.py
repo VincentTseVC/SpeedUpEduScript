a83643de79045ce133dca1b5d68318cbc6663bba
VincentTse <from_vincent@hotmail.com>
2022-10-25 15:53:05 +0800

    changed

--- game_functions.py
+++ game_functions.py
@@ -1,25 +1,33 @@
 
-def func1(a:int,  b:  str)-> str:
+
+
+def func1(a: int, b: str) -> str:
     """aaa
 
-    >>>func1(1,'a')
+    >>> func1(1, 'a')
     """
-    a=a+b or  2
-    c = a   -b %1
-    d= 1*2
-    word ="s"
+    a = a + b or 2
+    c = a - b % 1
+    d = 1 * 2
+    word = "s"
     return a
 
+
 def func2():
-    a = a+1
-    b *=3
-    return 1 ==2
+    a = a + 1
+    b *= 3
+    return 1 == 2
+
+
 def func3():
-    return 1>=   2  and   3>5
+    return 1 >= 2 and 3 > 5
+
 
 # test
 def func4():
     pass
+
+
 # test2
 # test3
 #
